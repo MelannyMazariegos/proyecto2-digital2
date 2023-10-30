6e8608f72c80c9c3fa78da33dac4ec581c03e6f0
MelannyMazariegos <maz21055@uvg.edu.gt>
2023-10-29 23:13:38 -0600

    Create proyecto2-digital2.ino

---
+++ proyecto2-digital2.ino
@@ -0,0 +1,27 @@
+#define RXp2 PD6 //Pines para la comunicacion entre esp32 y Tiva C
+#define TXp2 PD7
+const int guardar = PUSH2;
+const int medir = PUSH1;
+int latido;
+int mandar;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  pinMode(guardar, INPUT_PULLUP);
+  pinMode(medir, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   mandar = digitalRead(medir);
+   if(mandar == 0){
+    Serial2.println('3');
+   }
+   if (Serial2.available()>0){
+    latido = Serial2.parseInt();
+    String incomingData = Serial2.readStringUntil('\n');
+    Serial.print(latido);
+   }
+}
