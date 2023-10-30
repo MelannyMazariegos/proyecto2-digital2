2a51bad69befa35a626737378efff84eec418bbd
MelannyMazariegos <maz21055@uvg.edu.gt>
2023-10-30 13:43:50 -0600

    Update programaTivaC.ino

--- programaTivaC/programaTivaC.ino
+++ programaTivaC/programaTivaC.ino
@@ -7,7 +7,7 @@ int melodia[] = {262, 196, 196, 220, 196, 0, 247, 262};
 int duracionNotas[] = {4, 8, 8, 4, 4, 4, 4, 4};
 int gan_melody[] = {330, 392, 330, 392, 0, 392, 440, 440};
 int duraciones[] = {8, 8, 4, 4, 4, 4, 4, 4};
-const int BUZZER = 5;
+//const int BUZZER = 5;
 const int guardar = PUSH2;
 const int medir = PUSH1;
 int latido;
@@ -26,16 +26,16 @@ void loop() {
   mandar = digitalRead(medir);
   if(mandar == LOW){
    if (Serial2.available()>0){
-     latido = Serial2.parseInt();
-     String incomingData = Serial2.readStringUntil('\n');
-     Serial.println(latido);
+    latido = Serial2.parseInt();
+    String incomingData = Serial2.readStringUntil('\n');
+    Serial.println(latido);
+   }
+   //for(int i = 0; i < 8; i++){
+  //  int duracionNota = 1000/duracionNotas[i];
+    //tone(BUZZER, melodia[i],duracionNotas);
+//    int pausaEntreNotas = duracionNota * 1.30;
+  //  delay(pausaEntreNotas);
+    //noTone(BUZZER);
+ //  }
   }
-  for(int i = 0; i < 8; i++){
-    int duracionNota = 1000/duracionNotas[i];
-    tone(BUZZER, melodia[i],duracionNotas);
-    int pausaEntreNotas = duracionNota * 1.30;
-    delay(pausaEntreNotas);
-    noTone(BUZZER);
-  }
- }
 }
